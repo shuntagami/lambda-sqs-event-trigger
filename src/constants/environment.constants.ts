6454df1230f6a735f8096e7032460ecda02ae966
shuntagami <shuntagami23@gmail.com>
2023-01-04 01:20:12 +0900

    support localstack

--- src/constants/environment.constants.ts
+++ src/constants/environment.constants.ts
@@ -9,3 +9,9 @@ export const MESSAGE_QUEUE_URL = env["MESSAGE_QUEUE_URL"] as string;
 export const AWS_REGION = env["AWS_REGION"] as string;
 
 export const S3_BUCKET_NAME = env["S3_BUCKET_NAME"] as string;
+
+export const S3_ENDPOINT = env["S3_ENDPOINT"];
+
+export const AWS_ACCESS_KEY_ID = env["AWS_ACCESS_KEY_ID"];
+
+export const AWS_SECRET_ACCESS_KEY = env["AWS_SECRET_ACCESS_KEY "];
